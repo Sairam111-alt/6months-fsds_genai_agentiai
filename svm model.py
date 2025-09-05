d83465b941f2a7119b6e26e64eb92d843dc2292d
Sairam111-alt <ranaorton@gmail.com>
2025-09-05 21:22:55 +0530

    Non Linear Model-K Nearest Neighbour Model to predict the salary based on experience. X=6.5 years exp

--- svm model.py
+++ svm model.py
@@ -15,3 +15,11 @@ svr_regressor.fit(x,y)
 
 svr_model_pred= svr_regressor.predict([[6.5]])
 print(svr_model_pred)
+
+# Knn Model
+from sklearn.neighbors import KNeighborsRegressor
+knn_reg_model=KNeighborsRegressor(n_neighbors=3, weights='distance')
+knn_reg_model.fit(x,y)
+
+knn_reg_pred=knn_reg_model.predict([[6.5]])
+print(knn_reg_pred)
